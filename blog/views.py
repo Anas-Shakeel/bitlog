740c2dc78c14040a0f6c6fb1be603feece957193
anas-shakeel <srdranas456@gmail.com>
2025-03-06 17:20:28 +0500

    Return to referer

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Count
 from django.utils.text import slugify
-from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib import messages
+from django.http import HttpRequest
 
 from .models import BlogPost, Category, Tag, Comment, Save
 from .forms import BlogPostForm, CommentForm
@@ -253,8 +254,8 @@ def like_post(request, slug):
         else:
             # Unlike it.
             like.delete()
-
-    return redirect("blog_detail", slug=slug)
+    return redirect(request.META.get("HTTP_REFERER"))
+    # return redirect("blog_detail", slug=slug)
 
 
 # Comment on a post
